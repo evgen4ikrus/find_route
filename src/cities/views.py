5044cbabf366655d7b033bfa5d590528fe76d794
EvgeniySHE <ewgen-3347@yandex.ru>
2022-01-16 22:06:11 +0300

    3

--- src/cities/views.py
+++ src/cities/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from cities.models import City
 
-# Create your views here.
+def home(request):
+    qs = City.objects.all()
+    context = {'objects_list': qs}
+    return render(request, 'cities/home.html', context)
